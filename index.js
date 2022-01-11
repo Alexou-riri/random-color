3d8e269b060e44409a972a46745bed84ce3c146f
Alex <alexandra.rieux@gmx.com>
2022-01-11 20:33:53 +0100

    Add function loop

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-//
+import randomColor from 'randomcolor'; // import the script
+
+const name = process.argv[2];
+console.log(name);
+
+/* const columns = 31;
+const row = 9;
+let i;
+let y;
+
+for (i = 0; i < columns; i++) {
+  for (y = 0; y < row; y++) {
+    console.log('#');
+  }
+  console.log('#');
+} */
+
+/* let square = `
+###############################
+###############################
+###############################
+###############################
+###############################
+###############################
+###############################
+###############################
+###############################`;
+
+console.log(square); */
+
+// Java Program to Print Square Pattern
+
+const n = 9;
+const y = 31;
+let string = '';
+
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < y; j++) {
+    string += '#';
+  }
+  string += '\n';
+}
+
+console.log(string);
+
+console.log();
